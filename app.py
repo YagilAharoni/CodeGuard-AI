3c8df54ce57070d69d53f05ed268c81bf42d2d08
Yagil Aharoni <yagilaharoni@gmail.com>
2026-04-04 15:08:56 +0300

    update

--- app.py
+++ app.py
@@ -231,4 +231,4 @@ async def export_pdf_endpoint(request: Request, report_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main_api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
